3eb204a77d0464bb65bd12c7a6a132e7f5d328d1
Leong Chi Wei <chiwei0708@gmail.com>
2022-05-29 15:26:12 +0800

    added cors package

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require('cors');
 
 const tasksRoutes = require("./routes/tasks");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/tasks", tasksRoutes);
 
